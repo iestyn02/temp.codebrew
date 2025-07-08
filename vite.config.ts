c4eb87083d427a950a5cd914dfa18ba2c991b969
Iestyn Jones <iestyn02@gmail.com>
2025-07-08 14:58:46 +0100

    Boilerplate code in place

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,21 @@
-import { reactRouter } from "@react-router/dev/vite";
-import tailwindcss from "@tailwindcss/vite";
-import { defineConfig } from "vite";
-import tsconfigPaths from "vite-tsconfig-paths";
+import { reactRouter } from '@react-router/dev/vite';
+
+import react from '@vitejs/plugin-react'
+
+import tailwindcss from '@tailwindcss/vite';
+
+import { defineConfig } from 'vite';
+
+import tsconfigPaths from 'vite-tsconfig-paths';
+
+import path from 'path'
 
 export default defineConfig({
+  root: 'frontend',
   plugins: [tailwindcss(), reactRouter(), tsconfigPaths()],
+  build: {
+    outDir: '../dist',
+    emptyOutDir: true
+  },
+  publicDir: path.resolve(__dirname, 'frontend/public')
 });
